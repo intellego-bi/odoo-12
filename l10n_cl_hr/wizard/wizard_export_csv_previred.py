f264a925ac2617afd95a62bf5066ecf7a988e326
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-18 08:35:42 -0300

    Update wizard_export_csv_previred.py

--- l10n_cl_hr/wizard/wizard_export_csv_previred.py
+++ l10n_cl_hr/wizard/wizard_export_csv_previred.py
@@ -604,5 +604,5 @@ class WizardExportCsvPrevired(models.TransientModel):
                     'file_name': "NominaPrevired_%s_%s_(%s).csv" % (file_year, file_month, self.indicadores_id.name),
                     })
         
-        file_lines = sum(1 for _ in open(content))        
+        file_lines = sum(1 for _ in open(self.file_name))        
         return self.show_view(u'Archivo Previred Generado %s' % (file_lines))
