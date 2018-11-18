44de28fa54ed1b99747bb19a7bc220396b8764d1
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-18 03:48:50 -0300

    Update wizard_export_csv_previred.py

--- l10n_cl_hr/wizard/wizard_export_csv_previred.py
+++ l10n_cl_hr/wizard/wizard_export_csv_previred.py
@@ -289,8 +289,8 @@ class WizardExportCsvPrevired(models.TransientModel):
 
         #date_start_format = datetime.strptime(date_start, DF).strftime("%m%Y")
         #date_stop_format = datetime.strptime(date_stop, DF).strftime("%m%Y")
-        date_start_format = datetime.strptime(date_start[:10], "%Y/%m/%d").strftime("%m%Y")
-        date_stop_format = datetime.strptime(date_stop[:10], "%Y/%m/%d").strftime("%m%Y")
+        date_start_format = datetime.strptime(date_start[:10], "%Y-%m-%d").strftime("%m%Y")
+        date_stop_format = datetime.strptime(date_stop[:10], "%Y-%m-%d").strftime("%m%Y")
 
         line_employee = []
         rut = ""
