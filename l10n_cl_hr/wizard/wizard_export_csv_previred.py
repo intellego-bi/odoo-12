1a5608ca1624ca9ee3087d2ad638eb14d6daf320
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-18 03:15:14 -0300

    Update wizard_export_csv_previred.py

--- l10n_cl_hr/wizard/wizard_export_csv_previred.py
+++ l10n_cl_hr/wizard/wizard_export_csv_previred.py
@@ -281,13 +281,16 @@ class WizardExportCsvPrevired(models.TransientModel):
         payslip_recs = payslip_model.search([('date_from','=',self.date_from),
                                              ])
 
-        #date_start = self.date_from
-        #date_stop = self.date_to
-        date_start = datetime.datetime.from_string(self.date_from)
-        date_stop = datetime.from_string(self.date_to)
+        date_start = self.date_from
+        date_stop = self.date_to
+        #date_start = datetime.datetime.from_string(self.date_from)
+        #date_stop = datetime.from_string(self.date_to)
 
-        date_start_format = datetime.strptime(date_start, DF).strftime("%m%Y")
-        date_stop_format = datetime.strptime(date_stop, DF).strftime("%m%Y")
+        #date_start_format = datetime.strptime(date_start, DF).strftime("%m%Y")
+        #date_stop_format = datetime.strptime(date_stop, DF).strftime("%m%Y")
+        date_start_format = datetime.datetime.strptime(date_start, DF).strftime("%m%Y")
+        date_stop_format = datetime.datetime.strptime(date_stop, DF).strftime("%m%Y")
+
         line_employee = []
         rut = ""
         rut_dv = ""
