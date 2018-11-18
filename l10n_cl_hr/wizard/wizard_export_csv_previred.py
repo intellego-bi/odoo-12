6a2b524184a8c5483f35600c41bb88f040e288c1
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-18 02:36:41 -0300

    Update wizard_export_csv_previred.py

--- l10n_cl_hr/wizard/wizard_export_csv_previred.py
+++ l10n_cl_hr/wizard/wizard_export_csv_previred.py
@@ -42,14 +42,14 @@ class WizardExportCsvPrevired(models.TransientModel):
     quotechar = '"'
     date_from = fields.Date('Fecha Inicial', required=True)
     date_to = fields.Date('Fecha Final', required=True)
-    file_data = fields.Binary('Archivo csv', filters=None, help="",)
+    file_data = fields.Binary('Archivo CSV', filters=None, help="", default="Previred.csv")
     file_name = fields.Char('Nombre de archivo', size=256, required=False, help="",)
 
-    _defaults = {
-        'date_from': lambda *a: time.strftime('%Y-%m-01'),
-        'date_to': lambda *a: str(datetime.now() + relativedelta.relativedelta(months=+1, day=1, days=-1))[:10],
+    #_defaults = {
+    #    'date_from': lambda *a: time.strftime('%Y-%m-01'),
+    #    'date_to': lambda *a: str(datetime.now() + relativedelta.relativedelta(months=+1, day=1, days=-1))[:10],
 
-    }
+    #}
 
 
     def getrecord_treeview(self, name):  
