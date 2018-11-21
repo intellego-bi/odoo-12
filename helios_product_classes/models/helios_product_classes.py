d2b8e77821e5bc4e2c078c0cf99e0f2cc3073408
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-21 14:41:05 -0300

    Update helios_product_classes.py

--- helios_product_classes/models/helios_product_classes.py
+++ helios_product_classes/models/helios_product_classes.py
@@ -17,7 +17,7 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+# 
 ###################################################################################
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
