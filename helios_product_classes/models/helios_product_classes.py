29a323516256e641a60db711494bbd2a087990d6
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-21 14:44:16 -0300

    Update helios_product_classes.py

--- helios_product_classes/models/helios_product_classes.py
+++ helios_product_classes/models/helios_product_classes.py
@@ -27,20 +27,13 @@ from odoo.tools.translate import _
 
 class Product(models.Model):
     _inherit = 'product.template'
+    unidades = fields.Float('N° Unidades', digits=dp.get_precision('Product Unit of Measure'), help="N° Unidades por Paquete de producto", copy=True)
+    largo = fields.Float('Largo (cm)', digits=dp.get_precision('Product Unit of Measure'), help="Largo del corte en CM", copy=True)	
+    ancho = fields.Float('Ancho (cm)', digits=dp.get_precision('Product Unit of Measure'), help="Ancho del corte en CM", copy=True)	
+    gramaje = fields.Float('Gramaje por m2', digits=dp.get_precision('Product Unit of Measure'), help="Gramos de peso por metro cuadrado", copy=True)	
+    lista = fields.Float('Lista', digits=dp.get_precision('Helios Product Class'), help="?", copy=True)	
+    ns = fields.Float('NS', digits=dp.get_precision('Helios Product Class'), help="?", copy=True)	
+    ecobond = fields.Float('Ecobond', digits=dp.get_precision('Helios Product Class'), help="?", copy=True)	
+    mx = fields.Float('MX', digits=dp.get_precision('Helios Product Class'), help="?", copy=True)
 
-	unidades = fields.Float('N° Unidades', digits=dp.get_precision('Product Unit of Measure'), help="N° Unidades por Paquete de producto", copy=True)
-	
-	largo = fields.Float('Largo (cm)', digits=dp.get_precision('Product Unit of Measure'), help="Largo del corte en CM", copy=True)
-	
-	ancho = fields.Float('Ancho (cm)', digits=dp.get_precision('Product Unit of Measure'), help="Ancho del corte en CM", copy=True)
-	
-	gramaje = fields.Float('Gramaje por m2', digits=dp.get_precision('Product Unit of Measure'), help="Gramos de peso por metro cuadrado", copy=True)
-	
-	lista = fields.Float('Lista', digits=dp.get_precision('Helios Product Class'), help="?", copy=True)
-	
-	ns = fields.Float('NS', digits=dp.get_precision('Helios Product Class'), help="?", copy=True)
-	
-	ecobond = fields.Float('Ecobond', digits=dp.get_precision('Helios Product Class'), help="?", copy=True)
-	
-	mx = fields.Float('MX', digits=dp.get_precision('Helios Product Class'), help="?", copy=True)
 
