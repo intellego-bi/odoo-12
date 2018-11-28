fd135094945fd1b4df21aff567bb16584bdf256c
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-28 03:21:31 -0300

    Update hr_payroll_finiquito.py

--- hr_gratuity_settlement/models/hr_payroll_finiquito.py
+++ hr_gratuity_settlement/models/hr_payroll_finiquito.py
@@ -63,15 +63,17 @@ class HrPayslip(models.Model):
         emp_id = contract_obj.browse(contract_ids[0].id).employee_id
         settle_obj = self.env['hr.settlements'].search([('employee_id', '=', emp_id.id), ('state', '=', 'approve')])
         for settle in settle_obj:
-            for result in res:
-                if result.get('code') == 'FIN':
-                    result['amount'] = settle.allowance
-                    result['hr_settlements_id'] = settle.id
+            if date_from <= settle.settle_date <= date_to: # and not settle.paid:
+                for result in res:
+                    if result.get('code') == 'FIN':
+                        result['amount'] = settle.allowance
+                        result['hr_settlements_id'] = settle.id
         for settle in settle_obj:
-            for result in res:
-                if result.get('code') == 'IAS':
-                    result['amount'] = settle.gratuity_amount
-                    result['hr_settlements_id'] = settle.id
+            if date_from <= settle.settle_date <= date_to: # and not settle.paid:
+                for result in res:
+                    if result.get('code') == 'IAS':
+                        result['amount'] = settle.gratuity_amount
+                        result['hr_settlements_id'] = settle.id
         return res
 
     @api.multi
