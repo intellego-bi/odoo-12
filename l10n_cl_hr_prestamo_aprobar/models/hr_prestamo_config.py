3e731e809ba52d195a0e2a37cd4d361602ab6dde
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-29 14:47:24 -0300

    Update hr_prestamo_config.py

--- l10n_cl_hr_prestamo_aprobar/models/hr_prestamo_config.py
+++ l10n_cl_hr_prestamo_aprobar/models/hr_prestamo_config.py
@@ -47,6 +47,6 @@ class AccConfig(models.TransientModel):
     def set_values(self):
         super(AccConfig, self).set_values()
         self.env['ir.config_parameter'].sudo().set_param('account.prestamo_approve', self.prestamo_approve)
-        self.env['ir.config_parameter'].sudo().set_param('account.emp_account_id', self.prestamo_approve)
-        self.env['ir.config_parameter'].sudo().set_param('account.treasury_account_id', self.prestamo_approve)
+        self.env['ir.config_parameter'].sudo().set_param('account.emp_account_id', self.emp_account_id)
+        self.env['ir.config_parameter'].sudo().set_param('account.treasury_account_id', self.treasury_account_id)
 
