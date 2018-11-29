3f215f7027631bf8b41fa24d993a62a3b954e133
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-29 19:08:15 -0300

    Update __init__.py

--- l10n_cl_hr_prestamo_aprobar/models/__init__.py
+++ l10n_cl_hr_prestamo_aprobar/models/__init__.py
@@ -21,5 +21,5 @@
 ###################################################################################
 from . import hr_prestamo_config
 from . import hr_prestamo_acc
-from . import res_config_settings
+#from . import res_config_settings
 
