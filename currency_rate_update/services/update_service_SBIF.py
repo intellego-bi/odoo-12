62a33c8b21b3d4c5a8e481f2d63c0e39f13b4af2
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-29 21:29:00 -0300

    CL Currency Rate Update API Settings

--- currency_rate_update/services/update_service_SBIF.py
+++ currency_rate_update/services/update_service_SBIF.py
@@ -1,9 +1,24 @@
-# © 2009 Camptocamp
-# © 2009 Grzegorz Grzelak
-# © 2018 Intellego-BI.com
-# © 2018 Rodolfo Bermudez Neubauer
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    Intellego-BI.com
+#    Copyright (C) 2017-TODAY Intellego Business Intelligence S.A.(<http://www.intellego-bi.com>).
+#    Author: Rodolfo Bermúdez Neubauer(<https://www.intellego-bi.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+###################################################################################
 from .currency_getter_interface import CurrencyGetterInterface
 
 from datetime import datetime
