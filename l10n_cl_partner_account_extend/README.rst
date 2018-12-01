e7c91ede69ac396b688c0684c10e231f95b6e853
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-11-30 22:00:43 -0300

    CL Partner Account Extend

---
+++ l10n_cl_partner_account_extend/README.rst
@@ -0,0 +1,32 @@
+# Odoo Chile - Extensión de Vistas de Partner
+[l10n_cl_partner_account_extend]
+
+## Odoo (ERP/CRM)
+ - Odoo 12 Localización chilena
+ - Módulos ajustados para versión 12 (Enterprise)
+
+## Scope 
+**Features list: Chilean Localization**
+
+    * Total a Cobrar y a Pagar en Maestro de Terceros
+    * Búsqueda por RUT en vista de lista
+    * Oculta campos irrelevantes para sólo Deudor o sólo Acreedor
+
+## Credits
+<p>
+<img width="200" alt="Logo Intellego-BI" src="https://i2.wp.com/intellego-bi.com/ws/wp-content/uploads/2016/05/Intellego-BI-112x35.jpg" />
+</p>
+
+**Intellego-BI.com** - https://intellego-bi.com
+ - Rodolfo Bermúdez Neubauer <odoo@intellego-bi.com>
+
+
+ ## To do
+ 
+ - Translations
+ 
+ 
+ ## Disclaimer
+ 
+ - Open source software: use at your own risk. 
+ - Public private repository. Not currently supporting third parties
---
+++ l10n_cl_partner_account_extend/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    Intellego-BI.com
+#    Copyright (C) 2017-TODAY Intellego Business Intelligence S.A.(<http://www.intellego-bi.com>).
+#    Author: Rodolfo Bermúdez Neubauer(<https://www.intellego-bi.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+###################################################################################
+{
+    'name': 'Chile :: Campos Adicionales Partner',
+    'summary': """Extiende vistas de maestro de Terceros para Chile""",
+    'version': '12.0.1.0.0',
+    'description': """Búsqueda por RUT, Monto a Pagar y a Cobrar y otros""",
+    'author': 'Intellego-BI.com',
+    'company': 'Intellego-BI.com',
+    'website': 'https://www.Intellego-BI.com',
+    'maintainer': 'Intellego-BI.com',
+    'category': 'Accounting',
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'data': [
+        'views/res_partner_account_extend.xml',
+    ],
+    'demo': [],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'auto_install': False,
+}
