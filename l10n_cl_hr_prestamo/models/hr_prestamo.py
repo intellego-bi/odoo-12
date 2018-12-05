986b7a6903f7870f7746140f4c860c584788abb4
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-05 20:48:58 -0300

    Update hr_prestamo.py

--- l10n_cl_hr_prestamo/models/hr_prestamo.py
+++ l10n_cl_hr_prestamo/models/hr_prestamo.py
@@ -23,6 +23,7 @@ from odoo import models, fields, api
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from odoo.exceptions import except_orm
+from odoo.exceptions import UserError
 
 
 class HrPrestamo(models.Model):
@@ -101,14 +102,14 @@ class HrPrestamo(models.Model):
                 for line in loan.prestamo_lines:
                     if not line.paid:
                         pending_total += line.amount
-                        pending_count +=
+                        #pending_count +=
         
         pend_total = str(pending_total)
         pend_count = str(pending_count)
 
         #if prestamo_count:
-        if total_pending:
-            raise except_orm("Error!", "This employee has '%s' pending installment(s) for a total of '%s'") % (pend_count, pend_total)
+        if pending_total:
+            raise UserError(_('Error!', 'This employee has %s pending installment(s) for a total of %s') % (pend_count, pend_total))
         else:
             values['name'] = self.env['ir.sequence'].get('hr.prestamo.seq') or ' '
             res = super(HrPrestamo, self).create(values)
