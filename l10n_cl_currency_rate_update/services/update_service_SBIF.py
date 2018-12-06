ec9cdad422f1899a309285f46bc186f74d857a75
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-06 05:53:53 -0300

    Update update_service_SBIF.py

--- l10n_cl_currency_rate_update/services/update_service_SBIF.py
+++ l10n_cl_currency_rate_update/services/update_service_SBIF.py
@@ -94,7 +94,7 @@ class SBIFGetter(CurrencyGetterInterface):
             currency_array.remove(main_currency)
         _logger.debug("SBIF currency rate service : connecting...")
 
-        self.config_read.get_apikey()
+        config_read.get_apikey()
         if config_read:
            apikey = config_read
         else:
