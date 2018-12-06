36d1cdc798e006098b2d7c1210505673155df45c
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-06 03:55:38 -0300

    Remove temp

--- temp_currency_rate_update/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import models
-from .services.currency_getter_interface import CurrencyGetterInterface
--- temp_currency_rate_update/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import currency_rate_update
-from . import company
-from . import res_config_settings
