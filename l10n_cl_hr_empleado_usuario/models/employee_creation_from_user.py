532fdeb9f252fd8b95a31aa86d791558021c4ccc
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-10 15:05:09 -0300

    Update employee_creation_from_user.py

--- l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
+++ l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
@@ -26,6 +26,8 @@ class ResUsersInherit(models.Model):
     _inherit = 'res.users'
 
     #formated_vat = fields.Char(translate=True, string='Printable VAT', store=True, help='Show formatted vat')
+    user_type = fields.Selection([('empl', 'Employee'), ('inte', 'Internal'), ('exte', 'External'), ('part', 'Partner')],
+                             string='User Type', default='empl')
     firstname = fields.Char("Firstname")
     last_name = fields.Char("Last Name")
     middle_name = fields.Char("Middle Name", help='Employees middle name')
