e401b1fd3817dd63caf2631f12d506144dc0a752
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-11 15:56:17 -0300

    Update employee_creation_from_user.py

--- l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
+++ l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
@@ -124,20 +124,22 @@ class ResUsersInherit(models.Model):
 
     #@api.multi
     @api.onchange('firstname', 'mothers_name', 'middle_name', 'last_name', 'type_id', 'gender', 'country_id', 'department_id', 'identification_id')
-    def update(self, vals):
+    def update(self):
         """This code is to update an employee while updating a user."""
-        vals['name'] = self._get_computed_name(
-                    vals['last_name'], vals['firstname'], vals['mothers_name'], vals['middle_name'])
-        result = super(ResUsersInherit, self).write(vals)
-        result['employee_id'] = self.env['hr.employee'].sudo().write({'name': vals['name'],
-                                                                      'firstname': vals['firstname'],
-                                                                      'middle_name': vals['middle_name'],
-                                                                      'last_name': vals['last_name'],
-                                                                      'mothers_name': vals['mothers_name'],
-                                                                      'type_id': vals['type_id'],
-                                                                      'gender': vals['gender'],
-                                                                      'country_id': vals['country_id'],
-                                                                      'department_id': vals['department_id'],
-                                                                      'identification_id': vals['identification_id']})
-        return result
+        self.write({'mothers_name': 'mothers_name'})
+        #self.write({'mothers_name': 'mothers_name'})
+        #vals['name'] = self._get_computed_name(
+        #            vals['last_name'], vals['firstname'], vals['mothers_name'], vals['middle_name'])
+        #result = super(ResUsersInherit, self).write(vals)
+        #result['employee_id'] = self.env['hr.employee'].sudo().write({'name': vals['name'],
+        #                                                              'firstname': vals['firstname'],
+        #                                                              'middle_name': vals['middle_name'],
+        #                                                              'last_name': vals['last_name'],
+        #                                                              'mothers_name': vals['mothers_name'],
+        #                                                              'type_id': vals['type_id'],
+        #                                                              'gender': vals['gender'],
+        #                                                              'country_id': vals['country_id'],
+        #                                                              'department_id': vals['department_id'],
+        #                                                              'identification_id': vals['identification_id']})
+        #return result
 
