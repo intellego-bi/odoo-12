50e81a32b2103eb6f9f06900265d407cc4a514fb
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-11 20:46:40 -0300

    Update employee_creation_from_user.py

--- l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
+++ l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
@@ -97,6 +97,7 @@ class ResUsersInherit(models.Model):
                                                                        'country_id': vals['country_id'],
                                                                        'department_id': vals['department_id'],
                                                                        'identification_id': vals['identification_id'],
+                                                                       'vat': vals['identification_id'],
                                                                        'address_home_id': result['partner_id'].id})
         return result
 
