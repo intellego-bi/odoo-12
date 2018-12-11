c44459528404ac72f430c2aa679c69c68bc1570c
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-11 16:05:38 -0300

    Update employee_creation_from_user.py

--- l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
+++ l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
@@ -126,8 +126,7 @@ class ResUsersInherit(models.Model):
     @api.onchange('firstname', 'mothers_name', 'middle_name', 'last_name', 'type_id', 'gender', 'country_id', 'department_id', 'identification_id')
     def update(self):
         """This code is to update an employee while updating a user."""
-        for user in self:
-            self.sudo().write({'mothers_name': self.mothers_name})
+        self.sudo().write({'mothers_name': vals['mothers_name']})
         #self.write({'mothers_name': 'mothers_name'})
         #vals['name'] = self._get_computed_name(
         #            vals['last_name'], vals['firstname'], vals['mothers_name'], vals['middle_name'])
