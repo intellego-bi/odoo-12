93d8d9c4af5e3b082e07e9e8f3638a0af70b4152
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-11 20:11:26 -0300

    HR Employee from User

--- l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
+++ l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
@@ -26,7 +26,6 @@ import re
 class ResUsersInherit(models.Model):
     _inherit = 'res.users'
 
-    formated_vat = fields.Char(translate=True, string='Printable VAT', store=True, help='Show formatted vat')
     identification_id = fields.Char(string='Identification No')
 
     user_type = fields.Selection([('empl', 'Employee'), ('inte', 'Internal')], string='User Type', default='empl')
@@ -80,27 +79,6 @@ class ResUsersInherit(models.Model):
             identification_id[0:2], identification_id[2:5], identification_id[5:8],
             identification_id[-1])
 
-    #@api.onchange('identification_id')
-    #def check_identification_id_cl (self):
-    #    body, vdig = '', ''
-    #    if len(self.identification_id) > 9:
-    #        self.identification_id = self.identification_id.replace('-','',1).replace('.','',2)
-    #    if len(self.identification_id) != 9:
-    #        raise UserError(u'El Rut no tiene formato')
-    #    else:
-    #        body, vdig = self.identification_id[:-1], self.identification_id[-1].upper()
-    #    try:
-    #        vali = range(2,8) + [2,3]
-    #        operar = '0123456789K0'[11 - (
-    #            sum([int(digit)*factor for digit, factor in zip(
-    #                body[::-1],vali)]) % 11)]
-    #        if operar == vdig:
-    #            return True
-    #        else:
-    #            raise UserError(u'El Rut no tiene formato')
-    #    except IndexError:
-    #        raise UserError(u'El Rut no tiene formato')
-
 
     @api.model
     def create(self, vals):
@@ -121,6 +99,28 @@ class ResUsersInherit(models.Model):
                                                                        'identification_id': vals['identification_id'],
                                                                        'address_home_id': result['partner_id'].id})
         return result
+
+
+    #@api.onchange('identification_id')
+    #def check_identification_id_cl (self):
+    #    body, vdig = '', ''
+    #    if len(self.identification_id) > 9:
+    #        self.identification_id = self.identification_id.replace('-','',1).replace('.','',2)
+    #    if len(self.identification_id) != 9:
+    #        raise UserError(u'El Rut no tiene formato')
+    #    else:
+    #        body, vdig = self.identification_id[:-1], self.identification_id[-1].upper()
+    #    try:
+    #        vali = range(2,8) + [2,3]
+    #        operar = '0123456789K0'[11 - (
+    #            sum([int(digit)*factor for digit, factor in zip(
+    #                body[::-1],vali)]) % 11)]
+    #        if operar == vdig:
+    #            return True
+    #        else:
+    #            raise UserError(u'El Rut no tiene formato')
+    #    except IndexError:
+    #        raise UserError(u'El Rut no tiene formato')
 
     #@api.onchange('mothers_name')
     #def on_change_user(self):
