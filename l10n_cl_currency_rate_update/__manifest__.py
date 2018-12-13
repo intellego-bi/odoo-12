8895d1273a65744a09d0c611a4ca1a1b2d95c8a6
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-12 22:56:52 -0300

    Update __manifest__.py

--- l10n_cl_currency_rate_update/__manifest__.py
+++ l10n_cl_currency_rate_update/__manifest__.py
@@ -1,10 +1,35 @@
-# © 2008-2016 Camptocamp
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    © 2008-2016 Camptocamp
+#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+#
+#    Intellego-BI.com
+#    Copyright (C) 2017-TODAY Intellego Business Intelligence S.A.(<http://www.intellego-bi.com>).
+#    Author: Rodolfo Bermúdez Neubauer(<https://www.intellego-bi.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+###################################################################################
 {
-    "name": "Currency Rate Update",
-    "version": "11.0.1.0.0",
-    "author": "Camptocamp,Odoo Community Association (OCA)",
-    "website": "http://camptocamp.com",
+    "name": "Chile :: Currency Rate Update",
+    'summary': 'Updates Chilean currency rates (USD, EUR, UF and UTM) from SBIF',
+    "version": "12.0.1.0.0",
+    "author": "Intellego-BI.com, Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://www.Intellego-BI.com",
+    'maintainer': 'Intellego-BI.com',
     "license": "AGPL-3",
     "category": "Financial Management/Configuration",
     "depends": [
