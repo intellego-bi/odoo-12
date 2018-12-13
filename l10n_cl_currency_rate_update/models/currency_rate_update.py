02269ef3aa934f05a042d6412ab8fb03bcd3f632
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-13 06:36:28 -0300

    Update currency_rate_update.py

--- l10n_cl_currency_rate_update/models/currency_rate_update.py
+++ l10n_cl_currency_rate_update/models/currency_rate_update.py
@@ -201,8 +201,8 @@ class CurrencyRateUpdateService(models.Model):
                         if curr == main_currency:
                             continue
                         #if curr == 'UTM':
-                            raise UserError(
-                                 _('Rate Name: %s') % rate_name)
+                        #    raise UserError(
+                        #         _('Rate Name: %s') % rate_name)
                         rates = rate_obj.search([
                             ('currency_id', '=', curr.id),
                             ('company_id', '=', company.id),
