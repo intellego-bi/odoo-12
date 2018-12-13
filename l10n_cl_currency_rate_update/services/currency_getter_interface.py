bd8fa468ca8dabdff228afa8bfdd226975b24eb4
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-13 05:10:08 -0300

    Update currency_getter_interface.py

--- l10n_cl_currency_rate_update/services/currency_getter_interface.py
+++ l10n_cl_currency_rate_update/services/currency_getter_interface.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api, _
 import logging
 
 from datetime import datetime
-from datetime import date, timedelta
+from datetime import timedelta
 #from odoo import fields, _
 from odoo.exceptions import UserError
 
