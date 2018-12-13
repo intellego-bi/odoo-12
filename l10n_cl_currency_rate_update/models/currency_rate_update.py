65c165185b17f02038fef61b72d5db1fdc1d7b22
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2018-12-13 06:22:36 -0300

    Update currency_rate_update.py

--- l10n_cl_currency_rate_update/models/currency_rate_update.py
+++ l10n_cl_currency_rate_update/models/currency_rate_update.py
@@ -191,11 +191,11 @@ class CurrencyRateUpdateService(models.Model):
                     #raise UserError(
                     #                _('Curr: %s Main: %s Delta: %s API %s') % (curr_to_fetch, main_currency.name, srv.max_delta_days, sbif_api_key))
 
-                    #rate_name = \
-                    #    fields.Datetime.to_string(datetime.utcnow().replace(
-                    #        hour=0, minute=0, second=0, microsecond=0))
-                    rate_name = \    
-                        datetime.to_string((date.today() - timedelta(1)).replace(hour=0, minute=0, second=0, microsecond=0))
+                    rate_name = \
+                        fields.Datetime.to_string((datetime.utcnow() - timedelta(1)).replace(
+                            hour=0, minute=0, second=0, microsecond=0))
+                    #rate_name = \    
+                    #    datetime.to_string((date.today() - timedelta(1)).replace(hour=0, minute=0, second=0, microsecond=0))
 
                     for curr in srv.currency_to_update:
                         if curr == main_currency:
